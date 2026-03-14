1e929d438da512598c06e7e4e7a2f108089a4e03
phumpatp <70514918+phumpatp@users.noreply.github.com>
2026-03-14 14:55:36 +0700

    Add files via upload

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBasePath = new System.Windows.Forms.TextBox();
+            this.txtFind = new System.Windows.Forms.TextBox();
+            this.txtReplace = new System.Windows.Forms.TextBox();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtBasePath
+            // 
+            this.txtBasePath.Location = new System.Drawing.Point(28, 25);
+            this.txtBasePath.Name = "txtBasePath";
+            this.txtBasePath.Size = new System.Drawing.Size(302, 20);
+            this.txtBasePath.TabIndex = 0;
+            this.txtBasePath.Text = "D:\\@Work\\@Other Project\\rename_folder_bat\\OUTPUT";
+            // 
+            // txtFind
+            // 
+            this.txtFind.Location = new System.Drawing.Point(28, 71);
+            this.txtFind.Name = "txtFind";
+            this.txtFind.Size = new System.Drawing.Size(302, 20);
+            this.txtFind.TabIndex = 1;
+            this.txtFind.Text = "H:\\STATEMENT";
+            // 
+            // txtReplace
+            // 
+            this.txtReplace.Location = new System.Drawing.Point(28, 111);
+            this.txtReplace.Name = "txtReplace";
+            this.txtReplace.Size = new System.Drawing.Size(302, 20);
+            this.txtReplace.TabIndex = 2;
+            this.txtReplace.Text = "nfs://FS_TESTAFP2PDF_ESTATEMENT_MASTER/STATEMENT";
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(355, 25);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(75, 23);
+            this.btnProcess.TabIndex = 3;
+            this.btnProcess.Text = "Process";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 148);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.txtReplace);
+            this.Controls.Add(this.txtFind);
+            this.Controls.Add(this.txtBasePath);
+            this.Name = "frmMain";
+            this.Text = "RenameBatchJob";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBasePath;
+        private System.Windows.Forms.TextBox txtFind;
+        private System.Windows.Forms.TextBox txtReplace;
+        private System.Windows.Forms.Button btnProcess;
+    }
+}
+
---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System;
+using System.Globalization;
+
+
+
+namespace WindowsFormsApp2
+{
+    public partial class frmMain : Form
+    {
+        string formattedDate;
+        string searchDate;
+        string sourcePath;
+        string destPath;
+        string indexPath;
+        string statementPath;
+        string folderSource;
+        string folderTarget;
+        string fullIndexPath;
+        string fullStatementPath;
+        string folderYear;
+
+        public frmMain()
+        {
+            InitializeComponent();
+            changeAndReplace();
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            changeAndReplace();
+
+        }
+
+
+        private void changeAndReplace()
+            {
+            try
+            {
+                //MessageBox.Show("OK");
+
+                //Directory.Move("d:\\xx11", "d:\\yyy");
+                //MessageBox.Show("OK");
+                CultureInfo cultureInfo = CultureInfo.CurrentCulture;
+
+                // Get the calendar associated with the current culture
+                Calendar calendar = cultureInfo.DateTimeFormat.Calendar;
+
+                // Display the calendar type
+                Console.WriteLine($"Calendar type: {calendar.GetType().Name}");
+
+
+                // If you need more details about the calendar, you can use different properties or methods
+                // For example, if it's the Gregorian calendar:
+                if (calendar is GregorianCalendar)
+                {
+                    //Console.WriteLine("Using the Gregorian Calendar.");
+                    int currentYear = calendar.GetYear(DateTime.Now);
+                    int thaiBuddhistYear = currentYear + 543;
+
+                    String currentdate = DateTime.Now.ToString("yyMMDD");
+                    DateTime sysDate = DateTime.Now;
+                    formattedDate = sysDate.ToString("YYMMdd");
+                    formattedDate = thaiBuddhistYear.ToString().Substring(2, 2) + formattedDate.Substring(2, 4);
+                    destPath = formattedDate;
+                    sourcePath = currentYear.ToString().Substring(2, 2) + formattedDate.Substring(2, 4);
+                    folderYear = thaiBuddhistYear.ToString().Substring(2, 2);
+                }
+                else
+                {
+                    Console.WriteLine("Using a different calendar.");
+                }
+
+                //MessageBox.Show("OK");
+                searchDate = formattedDate;
+                // Get the application path
+                string appPath = Application.StartupPath;
+
+                string masterpath = "";
+                string replacepath = "";
+
+                //MessageBox.Show(appPath);
+                Console.WriteLine(appPath);
+                // Define the config file name
+                string configFileName = "config.cfg";
+
+                // Combine the app path with the config file name
+                string configPath = Path.Combine(appPath, configFileName);
+
+                // Check if the file exists
+                if (File.Exists(configPath))
+                {
+                    // Read the content of the config file
+                    string[] lines = File.ReadAllLines(configPath);
+
+
+                    string[] tempstring = lines[0].Split('=');
+
+                    //get statement path
+
+                    statementPath = tempstring[1];
+                    //MessageBox.Show(indexPath);
+
+                    //get index path
+                    tempstring = lines[1].Split('=');
+                    indexPath = tempstring[1];
+
+                    //MessageBox.Show(statementPath);
+
+                    //get master path
+                    tempstring = lines[2].Split('=');
+
+                    masterpath = tempstring[1];
+                    //MessageBox.Show(masterpath);
+
+                    //get replace path
+                    tempstring = lines[3].Split('=');
+
+                    replacepath = tempstring[1];
+                    //MessageBox.Show(replacepath);
+
+                }
+                else
+                {
+                    Console.WriteLine($"Config file not found: {configPath}");
+                }
+
+                //MessageBox.Show("OK");
+
+                //rename folder and replace string of index file
+                // ตรวจสอบว่าโฟลเดอร์ต้นทางมีอยู่หรือไม่
+                //MessageBox.Show(indexPath + '\\' + sourcePath);
+                if (Directory.Exists(indexPath + '\\' + sourcePath))
+                {
+                    // เปลี่ยนชื่อโฟลเดอร์
+                    //MessageBox.Show("OK");
+                    sourcePath = indexPath + '\\' + sourcePath;
+                    destPath = indexPath + '\\' + destPath;
+                    Directory.Move(sourcePath, destPath);
+                    Console.WriteLine("โฟลเดอร์ถูกเปลี่ยนชื่อสำเร็จ!");
+                }
+                else
+                {
+                    Console.WriteLine("โฟลเดอร์ต้นทางไม่มีอยู่." + indexPath + '\\' + sourcePath);
+                }
+
+                // Find folder with sysdate format YYMMDD
+                var folders = Directory.GetDirectories(indexPath, "*" + searchDate + "*");
+                Console.WriteLine(indexPath+"\\"+ searchDate);
+                if (!folders.Any())
+                {
+                    Console.WriteLine("No folders found for today's date.");
+                    //return;
+                }
+
+                foreach (var folder in folders)
+                {
+                    // Get all files in the folder
+                    var files = Directory.GetFiles(folder);
+
+                    foreach (var file in files)
+                    {
+                        // Replace content in each file
+                        string content = File.ReadAllText(file);
+                        string updatedContent = content.Replace(masterpath, replacepath);
+
+                        File.WriteAllText(file, updatedContent);
+                    }
+                }
+
+
+                // ค้นหาโฟลเดอร์ที่ขึ้นต้นด้วย "DD"
+                //fullStatementPath = statementPath;
+                //D:\@Work\@Other Project\rename_folder_bat\OUTPUT\STATEMENT
+                string[] subfolders = Directory.GetDirectories(statementPath + "\\" + folderYear);
+                foreach (string folder in subfolders)
+                {
+                    // รับชื่อโฟลเดอร์
+                    string folderName = Path.GetFileName(folder);
+
+                    string[] subfolders1 = Directory.GetDirectories(statementPath + "\\" + folderYear + "\\" + folderName);
+                    foreach (string folder1 in subfolders1)
+                    {
+                        // เปลี่ยนชื่อจาก "DD..." เป็น "xx..."
+                        string folderName1 = Path.GetFileName(folder1);
+                        if (folderName1.StartsWith("DD"))
+                        {
+                            if (folderName1.Equals("DD11"))
+                            {
+                                folderTarget = folderName1.Replace("DD11", "04");
+                            }
+                            if (folderName1.Equals("DD12"))
+                            {
+                                folderTarget = folderName1.Replace("DD12", "08");
+                            }
+                            if (folderName1.Equals("DD13"))
+                            {
+                                folderTarget = folderName1.Replace("DD13", "12");
+                            }
+                            if (folderName1.Equals("DD14"))
+                            {
+                                folderTarget = folderName1.Replace("DD14", "16");
+                            }
+                            if (folderName.Equals("DD15"))
+                            {
+                                folderTarget = folderName1.Replace("DD15", "20");
+                            }
+                            if (folderName1.Equals("DD16"))
+                            {
+                                folderTarget = folderName1.Replace("DD16", "24");
+                            }
+                            if (folderName1.Equals("DD17"))
+                            {
+                                folderTarget = folderName1.Replace("DD17", "28");
+                            }
+                            if (folderName1.Equals("DD18"))
+                            {
+                                folderTarget = folderName1.Replace("DD18", "01");
+                            }
+
+                            //MessageBox.Show(statementPath);
+                            // DD11-DD18
+                            string oriFolderPath = Path.Combine(statementPath + "\\" + folderYear + "\\" + folderName, folder1);
+                            //convert to 01-28
+                            string newFolderPath = Path.Combine(statementPath + "\\" + folderYear + "\\" + folderName, folderTarget);
+
+                            Console.WriteLine(folderTarget + '|' + newFolderPath);
+                            if (Directory.Exists(newFolderPath))
+                            {
+                                // เปลี่ยนชื่อโฟลเดอร์
+                                Console.WriteLine("Dup");
+                                // คัดลอกไฟล์ในโฟลเดอร์ต้นทาง
+                                foreach (string file in Directory.GetFiles(oriFolderPath))
+                                {
+                                    string fileName = Path.GetFileName(file);
+                                    string destFile = Path.Combine(newFolderPath, fileName);
+                                    File.Copy(file, destFile, overwrite: true);
+                                }
+
+                                Directory.Delete(oriFolderPath);
+                                // คัดลอกโฟลเดอร์ย่อยในโฟลเดอร์ต้นทาง
+                                //foreach (string subDir in Directory.GetDirectories(oriFolderPath))
+                                //{
+                                //    string subDirName = Path.GetFileName(subDir);
+                                //    string destSubDir = Path.Combine(newFolderPath, subDirName);
+                                //    CopyFolderContents(subDir, destSubDir);
+                                //}
+                                //Directory.Move(folder, newFolderPath);
+                            }
+                            else
+                            {
+                                Console.WriteLine("โฟลเดอร์ต้นทางไม่มีอยู่. rename folder" + newFolderPath);
+                                Directory.Move(folder, newFolderPath);
+                                Console.WriteLine($"เปลี่ยนชื่อโฟลเดอร์ {folderName} เป็น {folderTarget}");
+                                Console.WriteLine("โฟลเดอร์ถูกเปลี่ยนชื่อสำเร็จ!" + newFolderPath);
+                            }
+                        }
+                    }
+                }
+
+
+                Console.WriteLine("Processing completed successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+
+
+        /// <summary>
+        /// คัดลอกข้อมูลจากโฟลเดอร์ต้นทางไปยังปลายทาง
+        /// </summary>
+        /// <param name="sourcePath">Path ของโฟลเดอร์ต้นทาง</param>
+        /// <param name="destinationPath">Path ของโฟลเดอร์ปลายทาง</param>
+        static void CopyFolderContents(string sourcePath, string destinationPath)
+        {
+            // สร้างโฟลเดอร์ปลายทางหากยังไม่มี
+            Directory.CreateDirectory(destinationPath);
+
+            // คัดลอกไฟล์ในโฟลเดอร์ต้นทาง
+            foreach (string file in Directory.GetFiles(sourcePath))
+            {
+                string fileName = Path.GetFileName(file);
+                string destFile = Path.Combine(destinationPath, fileName);
+                File.Copy(file, destFile, overwrite: true);
+            }
+
+            // คัดลอกโฟลเดอร์ย่อยในโฟลเดอร์ต้นทาง
+            foreach (string subDir in Directory.GetDirectories(sourcePath))
+            {
+                string subDirName = Path.GetFileName(subDir);
+                string destSubDir = Path.Combine(destinationPath, subDirName);
+                CopyFolderContents(subDir, destSubDir);
+            }
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
