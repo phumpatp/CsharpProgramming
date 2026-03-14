cb86e20de7ef8c6ae90b551ae97b4d6e406eff20
phumpatp <70514918+phumpatp@users.noreply.github.com>
2026-03-14 14:56:01 +0700

    Add files via upload

---
+++ rnFolder2/src/rnFolder2/Process.java
@@ -0,0 +1,21 @@
+package rnFolder2;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+
+public class Process {
+
+	public static void main(String[] args) {
+		File source = new File("H:\\work-temp\\file");
+		File dest = new File("H:\\work-temp\\file2");
+		try {
+		    FileUtils.copyDirectory(source, dest);
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
+
+	}
+
+}
